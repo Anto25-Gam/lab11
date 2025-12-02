a95104f641887db812767ee6f3e12d021fb85a53
Antonio Emanuel Balas - antonioemanuel.balas@studio.unibo.it <antonioemanuel.balas@studio.unibo.it>
2025-12-02 15:57:09 +0100

    finisch es 1

--- 111-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
+++ 111-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
@@ -9,9 +9,12 @@ import java.awt.Toolkit;
 import java.io.Serial;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -46,11 +49,29 @@ public final class LambdaFilter extends JFrame {
          */
         IDENTITY("No modifications", Function.identity()),
         LOWER("Convert to lowercase", String::toLowerCase),
-        COUNT_CHARS("Count the number of chars",x -> Long.toString( x.chars().filter(y -> y != ' ').count() ) ),
-        COUNT_LINES("Count the number of lines",x -> Long.toString( x.lines().count())),
-        LIST_WORDS_ORDER("List all the words in alphabetical order",x -> Arrays.stream(x.split("\\s+")).sorted().toString()),
-        COUNT_EACH_WORD("Write the count for each word",x -> x);
-        
+        COUNT_CHARS("Count the number of chars", x -> Integer.toString(x.length())),
+        COUNT_LINES("Count the number of lines", x -> Long.toString( x.lines().count())),
+        LIST_WORDS_ORDER(
+            "List all the words in alphabetical order", 
+            x -> Arrays.stream(x.split("\\s+"))
+                .sorted()
+                .collect(Collectors.joining(" "))
+        ),
+        COUNT_EACH_WORD(
+            "Write the count for each word", 
+            x -> Arrays.stream(x.split("\\s+"))
+                .collect(
+                    Collectors.toMap(
+                        k -> k, 
+                        k -> 1, 
+                        Integer::sum
+                    )
+                )
+                .entrySet()
+                .stream()
+                .map(t -> t.getKey() + " -> " + Integer.toString(t.getValue()))
+                .collect(Collectors.joining("\n"))
+        );
 
         private final String commandName;
         private final Function<String, String> fun;
